e2b76365e601b9a9f821e990d209f234460643ed
JanNDen <jc@pntr.eu>
2021-02-22 14:23:56 +0200

    Database Connection

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,34 @@
 "use strict";
 
+// Connection
+const mongoose = require("mongoose");
+const db = mongoose.connect(process.env.DB, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const { Schema } = mongoose;
+
+// Model for issues
+const issueSchema = new Schema({
+  project: String,
+  issue_title: { type: String, required: true },
+  issue_text: { type: String, required: true },
+  created_by: { type: String, required: true },
+  assigned_to: String,
+  status_text: String,
+  open: Boolean,
+  created_on: Date,
+  updated_on: Date,
+});
+const IssueModel = mongoose.model("issue", issueSchema);
+
+// Model for projects
+const projectSchema = new Schema({
+  userId: { type: String, required: true },
+  issues: [issueSchema],
+});
+const ProjectModel = mongoose.model("project", projectSchema);
+
 module.exports = function (app) {
   app
     .route("/api/issues/:project")
